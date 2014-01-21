e2f17799a529beb09faab42b86aecfd82e35c070
Claudio Cherubino <claudiocherubino@gmail.com>
2014-01-20 17:37:53 -0800

    Initial import of Gmailer

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 gmailer
 =======
 
-A simple tool to send test emails using a Gmail or Google Apps account
+A simple tool to send test emails using a Gmail or Google Apps account.
+
+## Usage
+
+    python gmailer.py USER PASSWORD RECIPIENT FILE [--subject SUBJECT]
+
+Where:
+
+* USER is the email address of the Google account to be used to send emails (e.g. me@gmail.com).
+* PASSWORD is the password used to authenticate as the Google account.
+* RECIPIENT is the email address of the user to send the email to. It also supports a comma-separated list of values.
+* FILE is the location of the file containing the HTML body of the email to send.
+* SUBJECT is the optional email subject. If not provided, the default value will be "Gmailer test - DATETIME"
+
+## Examples
+
+Login to Google as `me@gmail.com` (password `abcde123`) and send the HTML content from `./templates/goto_action.html` to `you@gmail.com`: 
+
+    python gmailer.py me@gmail.com abcde123 you@gmail.com ./templates/goto_action.html
+
+Same as before, but with custom subject line:
+
+    python gmailer.py me@gmail.com abcde123 you@gmail.com ./templates/goto_action.html --subject "Sample email from Gmailer"
---
+++ gmailer.py
@@ -0,0 +1,55 @@
+import argparse
+import email
+import smtplib
+
+from datetime import datetime
+from email.mime.text import MIMEText
+
+GOOGLE_SMTP_SERVER = 'smtp.gmail.com'
+TLS_PORT = 587
+
+
+def smtp_login(username, password, server=GOOGLE_SMTP_SERVER, port=TLS_PORT):
+  server = smtplib.SMTP(server, port)
+  server.starttls()
+  server.login(username, password)
+  return server
+
+def create_email(sender, recipient, subject, body):
+  message = MIMEText(body, 'html')
+  message['Subject'] = subject
+  message['From'] = sender
+  message['To'] = recipient
+  return message
+
+
+# configure and parse command-line parameters
+parser = argparse.ArgumentParser(description='Send emails using a Google account.')
+parser.add_argument('user', help='Google account used to send emails')
+parser.add_argument('password', help='Password to authenticate the Google account')
+parser.add_argument('to', metavar='recipient',
+    help='Recipient email address')
+parser.add_argument('file', type=argparse.FileType('r'),
+    help='Location of the file containing the markup to send')
+parser.add_argument('--subject',
+    help='Email subject')
+args = parser.parse_args()
+
+# set the email subject if it is not provided
+if not args.subject:
+	args.subject = 'Gmailer test - ' + str(datetime.now())
+
+# login to the Google SMTP server
+server = smtp_login(args.user, args.password)
+
+# create the email message
+email_body = args.file.read()
+email = create_email(
+    sender=args.user, recipient=args.to, subject=args.subject, body=email_body)
+
+try:
+  server.sendmail(args.user, args.to, email.as_string())
+  print "Email sent successfully"
+except smtplib.SMTPException as e:
+  print "Unable to send email: " + e
+
